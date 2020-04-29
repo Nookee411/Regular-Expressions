b9a363ed8096afa568adde6af674676a07b240f6
Nookee411 <daniil43rus@list.ru>
2020-04-29 10:52:35 +0300

    Added extention methods for string

---
+++ Task1/Extention.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Task1
+{
+    public static class StringExtention
+    {
+
+        public static bool IsIP(this string s)
+        {
+            Regex reg = new Regex(@"((25[0-5]|2[0-4]\d|[01]?\d\d?){3}\.)(25[0-5]|2[0-4]\d|[01]?\d\d?)");
+            return reg.IsMatch(s);
+        }
+
+        public static bool IsGUID(this string s)
+        {
+            Regex reg = new Regex(@"\S{8}-?\S{4}-\S{4}-\S{12}");
+            return reg.IsMatch(s);
+        }
+
+        public static bool IsURL(this string s)
+        {
+            Regex reg = new Regex(@"(https?:\/\/)?(w{3}\.)?([A-Za-z1-9]){2,}\.[A-Za-z1-9]+([:]?\w{4})?((\/[A-Za-z1-9]*)*)\/?(#[A-Za-z1-9]*)?(\w*=\w*)?");
+            return reg.IsMatch(s);
+        }
+
+        public static bool IsCorrectDate(this string s)
+        {
+            Regex reg = new Regex(@"(((0?[1-9])|([12][0-8]))\/(0?2)|(((0?[1-9])|([12][0-9])|(30))\/((0?[468])|(12)|(10))|((0?[1-9])|([12][0-9])|(3[01]))\/((0?[13579])|11)))\/([1][6-9][0-9][0-9]|[2-9][0-9][0-9][0-9])|((((0?[1-9])|([12][0-9]))\/(0?2)|(((0?[1-9])|([12][0-9])|(30))\/((0?[468])|(12)|(10))|((0?[1-9])|([12][0-9])|(3[01]))\/((0?[13579])|11)))\/(((2[0-9])|(1[5-9]))((0[48])|([13579][26])|([28][0468]))))");
+            return reg.IsMatch(s);
+        }
+        public static bool IsPrice(this string s)
+        {
+            Regex reg = new Regex(@"\d{1,}(\.\d{0,2})? ?р(уб)?\.?");
+            return reg.IsMatch(s);
+        }
+
+        public static bool IsGoodPassword(this string s)
+        {
+            Regex reg = new Regex(@"((?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?!.* ).{8,})");
+            return reg.IsMatch(s);
+        }
+    }
+}
--- Task1/Program.cs
+++ Task1/Program.cs
@@ -13,61 +13,34 @@ namespace Task1
         {
             
             Console.WriteLine("Input String:");
-            string s = Console.ReadLine();
-            if (IsIP(s))
-                Console.WriteLine("This string is IP");
-           
-            if (IsGUID(s))
-                Console.WriteLine("This string is GUID");
-            
-            if (IsURL(s))
-                Console.WriteLine("This string is URL");
-           
-            if (IsCorrectDate(s))
-                Console.WriteLine("This string is Date");
-
-            if (IsPrice(s))
-                Console.WriteLine("This string is Price");
-
-            if (IsGoodPassword(s))
-                Console.WriteLine("This password is strong");
-
+            string s;
+            s = "127.0.0.1, 255.255.255.0.";
+            if (s.IsIP())
+                Console.WriteLine(s+ "\nS is ip\n");
+            s = "e02fd0e4-00fd-090A-ca30-0d00a0038ba0.";
+            if (s.IsGUID())
+                Console.WriteLine(s+"\nS is GUID\n");
+            s = "e02fd0e400fd090Aca300d00a0038ba0.";
+            if (s.IsGUID())
+                Console.WriteLine(s+"\nS is GUID\n");
+            s = "http://www.example.com";
+            if (s.IsURL())
+                Console.WriteLine(s + "\nS is URL");
+            s = "http://a.com.";
+            if (s.IsURL())
+                Console.WriteLine(s + "\nS is URL");
+            s = "29/02/2000";
+            if (s.IsCorrectDate())
+                Console.WriteLine(s + "\nS is correct date");
+            s = "23.78";
+            if (s.IsPrice())
+                Console.WriteLine(s + "\nS is correct price");
+            s = "C00l_Pass";
+            if (s.IsGoodPassword())
+                Console.WriteLine(s + "\nS is good password");
         }
 
-        static private bool IsIP(string s)
-        {
-            Regex reg = new Regex(@"\d{1,3}.?\d{1,3}.?\d{1,3}.?\d{1,3},? \d{1,3}.?\d{1,3}.?\d{1,3}.?\d{1,3}.?");
-            return reg.IsMatch(s);
-        }
 
-        static private bool IsGUID(string s)
-        {
-            Regex reg = new Regex(@"\S{8}-?\S{4}-\S{4}-\S{12}");
-            return reg.IsMatch(s);
-        }
-
-        static private bool IsURL(string s)
-        {
-            Regex reg = new Regex(@"(https?:\/\/)?(w{3}\.)?([A-Za-z1-9]){2,}\.[A-Za-z1-9]+([:]?\w{4})?((\/[A-Za-z1-9]*)*)\/?(#[A-Za-z1-9]*)?(\w*=\w*)?");
-            return reg.IsMatch(s);
-        }
-
-        static private bool IsCorrectDate(string s)
-        {
-            Regex reg = new Regex(@"(((0?[1-9])|([12][0-8]))\/(0?2)|(((0?[1-9])|([12][0-9])|(30))\/((0?[468])|(12)|(10))|((0?[1-9])|([12][0-9])|(3[01]))\/((0?[13579])|11)))\/([1][6-9][0-9][0-9]|[2-9][0-9][0-9][0-9])|((((0?[1-9])|([12][0-9]))\/(0?2)|(((0?[1-9])|([12][0-9])|(30))\/((0?[468])|(12)|(10))|((0?[1-9])|([12][0-9])|(3[01]))\/((0?[13579])|11)))\/(((2[0-9])|(1[5-9]))((0[48])|([13579][26])|([28][0468]))))");
-            return reg.IsMatch(s);
-        }
-        static private bool IsPrice(string s)
-        {
-            Regex reg = new Regex(@"\d{1,}(\.\d{0,2})? ?р(уб)?\.?");
-            return reg.IsMatch(s);
-        }
-
-        static private bool IsGoodPassword(string s)
-        {
-            Regex reg = new Regex(@"((?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?!.* ).{8,})");
-            return reg.IsMatch(s);
-        }
 
     }
 }
